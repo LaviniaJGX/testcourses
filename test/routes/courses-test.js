5942974e47d0075fff32cca4c6fe7ef887074c51
GUIXIN JIANG <840762603@qq.com>
2018-11-06 22:35:00 +0800

    Delete course endpoint tested

--- test/routes/courses-test.js
+++ test/routes/courses-test.js
@@ -326,7 +326,7 @@ describe('Courses', function (){
          });
     });
 
-	describe.only('PUT /courses/:id/:teacherName/Teacher',  () => {
+	describe('PUT /courses/:id/:teacherName/Teacher',  () => {
 		describe('when id is valid',function(){
               it('should return a message and change teacher name', function(done) {
 			       let course = { 
@@ -390,7 +390,7 @@ describe('Courses', function (){
          });
     });
 
-    describe('DELETE/courses/:id',  () => {
+    describe.only('DELETE/courses/:id',  () => {
 	     describe('when id is valid',function(){
 		      it('should return delete message ', function(done) {
 				   chai.request(server)
